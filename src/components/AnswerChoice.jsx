699a4ee1a92909e7b965c02adc8dd0c619ee5028
Student <student@Students-MacBook-Air.local>
2020-02-06 19:48:07 -0800

    code no longer broken! removed some comments that are now not really useful

--- src/components/AnswerChoice.jsx
+++ src/components/AnswerChoice.jsx
@@ -18,9 +18,9 @@ export default class Answer extends Component {
     render() {
         let answers = ["a", "b", "c", "d"];
         let row = [];
-        /*answers.forEach(function(element) {
+        answers.forEach(function(element) {
             row.push();
-        })*/
+        })
         return (
             <div>
                 <button className="choices" onClick={() => this.handleClick()}>{this.props.choice}</button>
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,26 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import Question from './Question.jsx';
-import firebase from 'clients/firebase.js';
-import { buildFirebase } from '../clients/firebase.js';
 
 // import components
 
-class Loader {
-  constructor(choices, correct_choice_index, question_text){
-    // Fill in this constructor
-    this.choices= choices;
-    this.correct = correct_choice_index;
-    this.qtext= question_text ? question_text : ":0";
-  }
-}
+// class Loader {
+//   constructor(choices, correct_choice_index, question_text){
+//     // Fill in this constructor
+//     this.choices= choices;
+//     this.correct = correct_choice_index;
+//     this.qtext= question_text ? question_text : ":0";
+//   }
+// }
 
-class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   var database = buildFirebase();
-  //   var databaseRef = database.ref("/questions");
-  //   databaseRef.once("value").then(function(data) {
-  //   const questions = data.val();
-  //   // Do something with the questions
-  //   console.log(questions);
-  //   });
-  // }
-  
+class App extends Component { 
 
   render() {
  
 
-    let q1 = new Loader(["48", "13", "52", "50"],3, "How many states are in the United States?");
+    //let q1 = new Loader(["48", "13", "52", "50"],3, "How many states are in the United States?");
     // let q1 = new Loader(["48", "13", "52", "50"],3, "How many states are in the United States?");
-    console.log(q1);
+    //console.log(q1);
       
     return (
       <div className="app">
