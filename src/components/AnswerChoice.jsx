da4078d486909acb4c4fddf55c3522cfb7ec4a1d
Student <student@Students-MacBook-Air.local>
2020-02-26 18:25:46 -0800

    final css touches

--- src/components/AnswerChoice.jsx
+++ src/components/AnswerChoice.jsx
@@ -1,38 +1,51 @@
 import React, { Component } from 'react';
 
 export default class Answer extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             correctAns: 5,
+            color: 'black',
+            choices : 'choices',
+           
         }
       }
-
-    handleClick() {
-        //do something to check the answer
-        this.answerCheck();
-        // toggle next button if aswer is correct
-    }
 
     answerCheck() {
         let ans = this.props.correct;
         console.log("correct answer choice: "+ans);
         let clickId = this.props.id;
-        console.log(clickId);
-        // this.setState({correctAns: ans});
-        // console.log(this.state.correctAns);
+        //console.log(clickId);
         if (ans === clickId) {
             alert("correct!");
+            this.setState({color: 'green'});
         } else {
             alert("incorrect! try again!");
+            this.setState({color: 'gray'});
         }
-        return this.state.correct;
+        //return this.state.correct;
+    }
+
+    handleClick = () => {
+        let ans = this.props.correct;
+        console.log("correct answer choice: "+ans);
+        let clickId = this.props.id;
+        //console.log(clickId);
+        if (ans === clickId) {
+            alert("correct!");
+            this.setState({color: 'green'});
+        } else {
+            alert("incorrect! try again!");
+            this.setState({color: 'gray'});
+        }
+       // this.answerCheck();
+        //this.setState({color: 'black'});
     }
 
     render() {
         return (
             <div>
-                <button id={this.clickId} className="choices" onClick={() => this.handleClick()}>{this.props.ansChoice}</button>
+                <button id={this.clickId} className={this.state.choices, this.state.color} onClick={this.handleClick}>{this.props.ansChoice}</button>
             </div>
         )
     }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,26 +2,17 @@ import React, { Component } from 'react';
 // import components
 import Answer from './AnswerChoice';
 
-// UNSUED : every new question: needs "new Loader"
-// class Loader {
-//   constructor(choices, correct_choice_index, question_text){
-//     // Fill in this constructor
-//     this.choices= choices;
-//     this.correct = correct_choice_index;
-//     this.qtext= question_text ? question_text : ":0";
-//   }
-// }
-
 class Question extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
         index: 0,
+        color: 'black',
     }
   }
 
-  handleClick() {
-    this.setState({index: this.state.index+1})
+  handleClick = () => {
+    this.setState({index: this.state.index+1, color: 'black'})
     console.log(this.state.index);
   }
   
@@ -29,20 +20,25 @@ class Question extends Component {
     let q = this.props.why;
     let qText = q[this.state.index].question_text;
     
-    let answers = q[this.state.index].choices;
-    let ansRow = [];
-    for (let i=0; i<answers.length; i++) {
-      ansRow.push(<Answer ansChoice={answers[i]} correct={q[this.state.index].correct_choice_index} id={i} />);
-    }
-
+    debugger;
+   
+    //for (let i=0; i<answers.length; i++) {
+     // ansRow.push(<Answer ansChoice={answers[i]} correct={q[this.state.index].correct_choice_index} id={i} />);
+   // }
+  
     return (
       <div>
         <h1 className="questionBox">{qText}</h1>
         <br></br>
-        {ansRow}
+        {
+          q[this.state.index].choices.map( (answer, i) => {
+            return (<Answer ansChoice={answer} correct={q[this.state.index].correct_choice_index} />)
+          })
+        }
         <br></br>
    
-        <button id="nextBtn" onClick={() => this.handleClick()}> next </button>
+        <button id="nextBtn" onClick={this.handleClick}> next </button>
+        {/* {<button> something </button>} */}
       </div>
     );
 
